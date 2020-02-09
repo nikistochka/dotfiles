a193f0c1a1c9046b6241af7bbb2b921f6cfef786
nikistochka <nikistochka@github.com>
2020-02-09 02:58:03 -0500

    Edited first run script

--- first_run_centos.sh
+++ first_run_centos.sh
@@ -2,38 +2,34 @@
 #
 #First run script for RHEL
 
-hostname="sanfrancisco"
 
-# Changing hostname...
-sudo hostnamectl set-hostname ${hostname}
-
-grep  "127.0.0.1 ${hostname}" /etc/hosts
-if [[ $? != 0 ]]; then
-  sudo echo "127.0.0.1 ${hostname}" >> /etc/hosts
-fi
-
-exit 1
-
-
-break
-echo "break"
 
 sudo yum update -y
 echo "Installing packages..."
-sudo yum install -y git htop atop iotop zsh
+sudo yum install -y git htop atop iotop zsh mc 
 
 # Install oh-my-zsh
+
 echo "Installing oh-my-zsh..."
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sudo sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
 # Enable ohmyzsh plugins
 # by editing plugins=(git docker docker-compose)
 
 # Install docker
-echo "Installing docker & docker-compose..."
-sudo amazon-linux-extras install docker
+sudo yum install -y yum-utils \
+  device-mapper-persistent-data \
+  lvm2
+
+sudo yum-config-manager \
+    --add-repo \
+    https://download.docker.com/linux/centos/docker-ce.repo
+
+sudo yum install -y docker-ce docker-ce-cli containerd.io
 sudo systemctl start docker
 sudo systemctl enable docker
-sudo usermod -aG docker ${USER}
+
+
 # Install docker-compose
 sudo curl -L "https://github.com/docker/compose/releases/download/1.25.3/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
