1ef1d6385a1db0b275764f2853dad31c0a90a433
nikistochka <nikistochka@github.com>
2020-03-06 09:05:57 +0000

    added kubectl

--- first_run_ubuntu.sh
+++ first_run_ubuntu.sh
@@ -9,35 +9,45 @@ sudo apt-get -y install git htop atop iotop zsh mc tmux
 
 # Install docker
 if ! docker --version > /dev/null; then
-sudo apt-get -y install \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    gnupg-agent \
-    software-properties-common
-
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-
-sudo add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
-
-sudo apt-get -y update
-
-sudo apt -y install docker-ce docker-ce-cli containerd.io
-
-sudo usermod -aG docker $USER
+  sudo apt-get -y install \
+      apt-transport-https \
+      ca-certificates \
+      curl \
+      gnupg-agent \
+      software-properties-common
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+  sudo add-apt-repository \
+     "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+     $(lsb_release -cs) \
+     stable"
+  sudo apt-get -y update
+  sudo apt-get -y install docker-ce docker-ce-cli containerd.io
+  sudo usermod -aG docker $USER
+else
+  echo "docker already installed"
 fi
 
 # Install docker-compose
 if ! docker-compose --version > /dev/null; then
-sudo curl -L "https://github.com/docker/compose/releases/download/1.25.3/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
+  sudo curl -L "https://github.com/docker/compose/releases/download/1.25.3/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+  sudo chmod +x /usr/local/bin/docker-compose
+else
+  echo "docker-compose already installed"
 fi
 
+# Install kubectl
+if ! kubectl version --client > /dev/null; then
+  sudo apt-get update && sudo apt-get install -y apt-transport-https
+  curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+  echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
+  sudo apt-get update
+  sudo apt-get install -y kubectl
+else
+  echo "kubectl already installed"
+fi
+
+# Add alias 'git lgb'
 echo "Adding alias git lgb to ~/.gitconfig"
-# Add 'git lgb'
 git config --global alias.lgb "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset%n' --abbrev-commit --date=relative --branches"
 
 # Copy config files
@@ -50,5 +60,5 @@ echo "Installing oh-my-zsh..."
 sudo sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 cp config_files/robbyrussell.zsh-theme ~/.oh-my-zsh/custom/themes/robbyrussell.zsh-theme
 
-# Enable ohmyzsh plugins
+# Enable oh-my-zsh plugins
 # by editing plugins=(git docker docker-compose)
